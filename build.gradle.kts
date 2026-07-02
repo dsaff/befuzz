0aefb3102f29e42882f81be3e318d7cedd373141
David Saff <david@saff.net>
2026-07-01 20:34:28 -0400

    Added a warning

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,3 +80,8 @@ artifacts {
 kotlin {
   jvmToolchain(17)
 }
+
+// SAFF:
+// w: The Kotlin source set nativeMain was configured but not added to any Kotlin compilation.
+// You can add a source set to a target's compilation by connecting it with the compilation's default source set using 'dependsOn'.
+// See https://kotl.in/connecting-source-sets
