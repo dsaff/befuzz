1e4004801fc35f81081a97cd5cc6608ad93d0652
David Saff <david@saff.net>
2026-08-08 08:22:19 -0400

    Move coin, add tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,6 +80,7 @@ kotlin {
   jvmToolchain(17)
 }
 
+// SAFF: fix?
 // w: The Kotlin source set nativeMain was configured but not added to any Kotlin compilation.
 // You can add a source set to a target's compilation by connecting it with the compilation's default source set using 'dependsOn'.
 // See https://kotl.in/connecting-source-sets
